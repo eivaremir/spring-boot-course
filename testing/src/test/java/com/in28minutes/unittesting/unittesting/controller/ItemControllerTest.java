0e9e3a7784ce2c51667ea54177d53d87ad5eeb09
Eivar Morales <eimorales@MOVMACTDI095.local>
2021-10-19 14:23:41 -0500

    step 08 unit test independent on business service

--- testing/src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemControllerTest.java
+++ testing/src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemControllerTest.java
@@ -1,14 +1,18 @@
 package com.in28minutes.unittesting.unittesting.controller;
 
+import com.in28minutes.unittesting.unittesting.business.ItemBusinessService;
+import com.in28minutes.unittesting.unittesting.model.Item;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,6 +21,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class ItemControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private ItemBusinessService businessService;
+
     @Test
     public void dummyitem_basic() throws Exception {
         RequestBuilder request = MockMvcRequestBuilders.get("/dummy-item").accept(MediaType.APPLICATION_JSON );
@@ -28,5 +36,23 @@ public class ItemControllerTest {
                 .andExpect(content().json("{\"id\":  1,\"price\":100,\"qty\":100}")) // this still succeeds
                 .andReturn();
 
+    }
+
+    @Test
+    public void itemFromBusinessService_basic() throws Exception {
+
+        // this make the UT independendent of the service
+        when(businessService.retrieveHardcodedItem()).thenReturn(
+                new Item(1,"ball",100,100)
+        );
+        /*
+        THIS FAILS BC THE BUSINESS SERVICE IS NOT LOADED BC ITS AN UNIT TEST
+        AS WE ONLY WANT TO TEST THE LOGIC AND NOT THE SERVICE, WE USE MOCKBEAN
+         */
+        RequestBuilder request = MockMvcRequestBuilders.get("/item-from-business-service").accept(MediaType.APPLICATION_JSON );
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\":  1,\"price\":100,\"qty\":100}")) // this still succeeds
+                .andReturn();
     }
 }
