6df23937b4432dbb23054096f68b105a401de94c
Eivar Morales <eimorales@MOVMACTDI095.local>
2021-10-19 11:22:12 -0500

    step 03 using response matchers to verify responses

--- testing/src/test/java/com/in28minutes/unittesting/unittesting/controller/HelloWorldControllerTest.java
+++ testing/src/test/java/com/in28minutes/unittesting/unittesting/controller/HelloWorldControllerTest.java
@@ -11,6 +11,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 //@RunWith(SpringRunner.class)
 @WebMvcTest(value= HelloWorldController.class)
@@ -21,8 +23,11 @@ public class HelloWorldControllerTest {
     public void helloWorld_basic() throws Exception {
         RequestBuilder request = MockMvcRequestBuilders.get("/hello-world").accept(MediaType.APPLICATION_JSON );
         // call "/hello-world"
-        MvcResult result = mockMvc.perform(request).andReturn();
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World")) // .json()
+                .andReturn();
         // verify "Hello World"
-        assertEquals("Hello World",result.getResponse().getContentAsString());
+        //assertEquals("Hello World",result.getResponse().getContentAsString());
     }
 }
