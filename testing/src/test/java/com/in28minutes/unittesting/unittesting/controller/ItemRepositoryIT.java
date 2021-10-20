45b787a3f36b8b91827039ec05b3dd3818f50456
Eivar Morales <eivaremir@gmail.com>
2021-10-20 16:20:00 -0500

    step 15integration test with @SpringBootTest

---
+++ testing/src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemRepositoryIT.java
@@ -0,0 +1,30 @@
+package com.in28minutes.unittesting.unittesting.controller;
+
+import com.in28minutes.unittesting.unittesting.data.ItemRepository;
+import com.in28minutes.unittesting.unittesting.model.Item;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ItemRepositoryIT {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void contextLoads() throws JSONException {
+        String  response=this.restTemplate.getForObject("/all-items-from-database",String.class);
+        JSONAssert.assertEquals("[{id:1001},{id:1002},{id:1003}]",response,false);
+
+    }
+}
